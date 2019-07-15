afd5a3a8b398851df6753a2999fc969b8f050a99
Jong in Park <whddls9632@naver.com>
2019-07-15 19:12:27 +0900

    First Commit

---
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/RiotOpenApiGetLeagueV4Application.java
@@ -0,0 +1,13 @@
+package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotOpenApiGetLeagueV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotOpenApiGetLeagueV4Application.class, args);
+	}
+
+}
