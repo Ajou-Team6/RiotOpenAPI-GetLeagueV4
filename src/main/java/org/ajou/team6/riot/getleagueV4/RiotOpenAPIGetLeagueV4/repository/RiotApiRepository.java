0222edc37f169e882c53120c5670b5abe211d7d3
Makjuho <37654702+MakJuho@users.noreply.github.com>
2019-07-16 14:16:42 +0900

    Modify params in repository (#13)
    * Modify_params_insertedstoredleague
    
    * Modify update.set

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
@@ -17,7 +17,6 @@ public class RiotApiRepository {
     @Autowired
     MongoTemplate mongoTemplate;
 
-
     public void insertStoredLeague(SetOfLeagueEntryDTO League) {
         mongoTemplate.insert(League);
     }
@@ -37,9 +36,9 @@ public class RiotApiRepository {
 
         Update update = new Update();
 
-        update.set("league", league);
+        update.set("leagueEntryDTOList", league);
 
-        mongoTemplate.updateFirst(query, update, "StoredLeague");
+        mongoTemplate.updateFirst(query, update, "setOfLeagueEntryDTO");
 
         return mongoTemplate.findOne(query, SetOfLeagueEntryDTO.class);
 
