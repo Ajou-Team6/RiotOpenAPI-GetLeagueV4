22c43c180495dd07522f73da8ba4e468cf77a73f
unknown <daddy2@ajou.ac.kr>
2019-07-16 13:19:40 +0900

    Repository Package - import Removed

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
@@ -1,7 +1,6 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.repository;
 
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
-import org.graalvm.compiler.lir.sparc.SPARCMove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
