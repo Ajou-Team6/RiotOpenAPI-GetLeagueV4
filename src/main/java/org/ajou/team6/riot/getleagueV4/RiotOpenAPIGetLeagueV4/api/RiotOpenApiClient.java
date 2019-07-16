b5094ebe52c15beff02143f31c1cb6f5dd23989b
Jong in Park <whddls9632@naver.com>
2019-07-16 12:54:57 +0900

    Initilaize Riot Open API Call Methods.

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/api/RiotOpenApiClient.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/api/RiotOpenApiClient.java
@@ -1,6 +1,10 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.api;
 
+import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
+import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,8 +32,19 @@ public class RiotOpenApiClient {
         }
     }
 
-    public String getOpenRiotKey() {
+    public String getOpenRiotApiKey() {
         return this.key;
+    }
+
+    public SummonerDTO requestEncryptedSummonerIdByName(String userName) {
+        if(key == null) loadOpenRiotApiKey();
+        String RealUrl = openEncryptedIdUrl + userName + requestParam;
+        return restTemplate.exchange(RealUrl, HttpMethod.GET, null, SummonerDTO.class, getOpenRiotApiKey()).getBody();
+    }
+
+    public List<LeagueEntryDTO> requestLeagueByEncryptedId(String id) {
+        String RealUrl = openLeagueUrl + id + requestParam;
+        return restTemplate.exchange(RealUrl,HttpMethod.GET, null, new ParameterizedTypeReference<List<LeagueEntryDTO>>() {}, getOpenRiotApiKey()).getBody();
     }
 
 }
