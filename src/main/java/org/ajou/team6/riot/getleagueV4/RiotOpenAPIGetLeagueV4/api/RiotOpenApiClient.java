ebbbc3bf7e966ed3d1259765b6900797a06dec0e
JonginPark <whddls9632@naver.com>
2019-07-16 12:39:59 +0900

    Make load and get methods about Riot Open Api Key. (#2)

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/api/RiotOpenApiClient.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/api/RiotOpenApiClient.java
@@ -1,4 +1,35 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Service
 public class RiotOpenApiClient {
+    private String key = null;
+    private String openEncryptedIdUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+    private String openLeagueUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/";
+    private String requestParam = "?api_key={key}";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public void loadOpenRiotApiKey() {
+        try {
+            key = new String ( Files.readAllBytes( Paths.get("key") ) );
+        }
+        catch (IOException e) {
+            this.key = null;
+        }
+    }
+
+    public String getOpenRiotKey() {
+        return this.key;
+    }
+
 }
