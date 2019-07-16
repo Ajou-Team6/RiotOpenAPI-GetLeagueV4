0584deaa7fc755c11a90008582806ffb701a419c
juhowoo <einjuho7@ajou.ac.kr>
2019-07-16 12:56:24 +0900

    insertStoredLeague

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
@@ -1,4 +1,26 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.repository;
 
+import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
+import org.graalvm.compiler.lir.sparc.SPARCMove;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class RiotApiRepository {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void insertStoredLeague(String summonerId) {
+        mongoTemplate.insert(summonerId);
+    }
+
+   
+
 }
