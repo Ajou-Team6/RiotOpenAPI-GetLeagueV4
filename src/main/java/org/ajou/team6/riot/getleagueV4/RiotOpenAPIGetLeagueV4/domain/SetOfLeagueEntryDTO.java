187cee45dd8c4bcf891f5651cdd62060c7bf6cf2
younho9 <dusgh121@gmail.com>
2019-07-18 12:29:16 +0900

    Change summonerId to summonerName to use as Id

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/domain/SetOfLeagueEntryDTO.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/domain/SetOfLeagueEntryDTO.java
@@ -1,11 +1,15 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain;
 
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Data
+@Builder
 public class SetOfLeagueEntryDTO {
-    String summonerId;
+    @Id
+    String summonerName;
     List<LeagueEntryDTO> leagueEntryDTOList;
 }
