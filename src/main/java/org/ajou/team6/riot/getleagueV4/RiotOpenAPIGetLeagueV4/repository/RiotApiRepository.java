83345c548b561ab763624082ceeda53a40a51a7b
younho9 <dusgh121@gmail.com>
2019-07-18 13:11:30 +0900

    Change to Repository method save LeagueEntryDTO Object

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/repository/RiotApiRepository.java
@@ -1,6 +1,6 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.repository;
 
-import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.SetOfLeagueEntryDTO;
+import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,7 +11,7 @@ public class RiotApiRepository {
     @Autowired
     MongoTemplate mongoTemplate;
 
-    public SetOfLeagueEntryDTO saveSetOfLeagueEntryDTO(SetOfLeagueEntryDTO leagueEntryDTOList) {
-        return mongoTemplate.save(leagueEntryDTOList);
+    public LeagueEntryDTO saveSetOfLeagueEntryDTO(LeagueEntryDTO leagueEntryDTO) {
+        return mongoTemplate.save(leagueEntryDTO);
     }
 }
