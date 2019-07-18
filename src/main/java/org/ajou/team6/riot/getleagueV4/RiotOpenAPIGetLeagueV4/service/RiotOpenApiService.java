548b23abf682c25ea8a4bf160ec3009b6c37a76e
younho9 <dusgh121@gmail.com>
2019-07-18 12:31:18 +0900

    delete find, insert, update method & create save method

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/service/RiotOpenApiService.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/service/RiotOpenApiService.java
@@ -1,13 +1,11 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.api.RiotOpenApiClient;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.SetOfLeagueEntryDTO;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.repository.RiotApiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,27 +13,20 @@ import java.util.List;
 @Service
 @Slf4j
 public class RiotOpenApiService {
-    @Autowired
-    ObjectMapper objectMapper;
+
     @Autowired
     RiotOpenApiClient riotOpenApiClient;
     @Autowired
     RiotApiRepository riotApiRepository;
-    @Autowired
-    MongoTemplate mongoTemplate;
 
-    public List<LeagueEntryDTO> getLeagueEntryDTOList(String summonerId) {
-        SetOfLeagueEntryDTO leagueEntryDTOList = new SetOfLeagueEntryDTO();
-        leagueEntryDTOList.setSummonerId(summonerId);
-        leagueEntryDTOList.setLeagueEntryDTOList(riotOpenApiClient.requestLeagueEntryDTOListByEncryptedId(summonerId));
+    public List<LeagueEntryDTO> getLeagueEntryDTOList(String summonerName) {
+        String id = riotOpenApiClient.requestEncryptedSummonerIdBySummonerName(summonerName).getId();
+        SetOfLeagueEntryDTO leagueEntryDTOList = SetOfLeagueEntryDTO.builder()
+                .summonerName(summonerName)
+                .leagueEntryDTOList(riotOpenApiClient.requestLeagueEntryDTOListByEncryptedId(id))
+                .build();
+        riotApiRepository.saveSetOfLeagueEntryDTO(leagueEntryDTOList);
 
-        if (riotApiRepository.findCurrentUserInfo(summonerId) == null) {
-            riotApiRepository.insertStoredLeague(leagueEntryDTOList);
-            log.info("LeagueEntryDTOList has been inserted successfully: " + summonerId);
-        } else {
-            riotApiRepository.updateStoredLeague(leagueEntryDTOList.getLeagueEntryDTOList(), summonerId);
-            log.info("LeagueEntryDTOList has been updated successfully: " + summonerId);
-        }
         return leagueEntryDTOList.getLeagueEntryDTOList();
     }
 }
