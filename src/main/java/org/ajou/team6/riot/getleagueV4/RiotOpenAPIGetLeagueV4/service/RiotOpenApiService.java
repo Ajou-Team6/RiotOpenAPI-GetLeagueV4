7c9839f452d94ad8a4a7fe721af31266483433c7
younho9 <dusgh121@gmail.com>
2019-07-18 13:13:45 +0900

    Change to Service method save LeagueEntryDTO Object List using repeat

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/service/RiotOpenApiService.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/service/RiotOpenApiService.java
@@ -3,7 +3,6 @@ package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.service;
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.api.RiotOpenApiClient;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
-import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.SetOfLeagueEntryDTO;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.repository.RiotApiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,13 +19,16 @@ public class RiotOpenApiService {
     RiotApiRepository riotApiRepository;
 
     public List<LeagueEntryDTO> getLeagueEntryDTOList(String summonerName) {
-        String id = riotOpenApiClient.requestEncryptedSummonerIdBySummonerName(summonerName).getId();
-        SetOfLeagueEntryDTO leagueEntryDTOList = SetOfLeagueEntryDTO.builder()
-                .summonerName(summonerName)
-                .leagueEntryDTOList(riotOpenApiClient.requestLeagueEntryDTOListByEncryptedId(id))
-                .build();
-        riotApiRepository.saveSetOfLeagueEntryDTO(leagueEntryDTOList);
-        log.info("League Positions has been upserted successfully: " + summonerName);
-        return leagueEntryDTOList.getLeagueEntryDTOList();
+        String encryptedId = riotOpenApiClient.requestEncryptedSummonerIdBySummonerName(summonerName).getId();
+        List<LeagueEntryDTO> leagueEntryDTOList = riotOpenApiClient.requestLeagueEntryDTOListByEncryptedId(encryptedId);
+
+        for(int i = 0; i < leagueEntryDTOList.size(); i++) {
+            LeagueEntryDTO.CompositeKey id = new LeagueEntryDTO.CompositeKey(summonerName, leagueEntryDTOList.get(i).getQueueType());
+            leagueEntryDTOList.get(i).setId(id);
+            riotApiRepository.saveSetOfLeagueEntryDTO(leagueEntryDTOList.get(i));
+            log.info("League Positions has been upserted successfully: " + summonerName + leagueEntryDTOList.get(i).getQueueType());
+        }
+
+        return leagueEntryDTOList;
     }
 }
