822754222f740b86fb5c22ce0e289f586b2ef3eb
younho9 <dusgh121@gmail.com>
2019-07-19 14:02:20 +0900

    Put in LeagueEntryDTO class to LeaguePosition class

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/service/RiotOpenApiService.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/service/RiotOpenApiService.java
@@ -2,7 +2,6 @@ package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.api.RiotOpenApiClient;
-import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeaguePosition;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.repository.RiotApiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +18,11 @@ public class RiotOpenApiService {
     @Autowired
     RiotApiRepository riotApiRepository;
 
-    public List<LeagueEntryDTO> getLeagueEntryDTOList(String summonerName) {
+    public List<LeaguePosition.LeagueEntryDTO> getLeagueEntryDTOList(String summonerName) {
         String encryptedId = riotOpenApiClient.requestEncryptedSummonerIdBySummonerName(summonerName).getId();
-        List<LeagueEntryDTO> leagueEntryDTOList = riotOpenApiClient.requestLeagueEntryDTOListByEncryptedId(encryptedId);
+        List<LeaguePosition.LeagueEntryDTO> leagueEntryDTOList = riotOpenApiClient.requestLeagueEntryDTOListByEncryptedId(encryptedId);
 
-        for (LeagueEntryDTO leagueEntryDTO : leagueEntryDTOList) {
+        for (LeaguePosition.LeagueEntryDTO leagueEntryDTO : leagueEntryDTOList) {
             LeaguePosition leaguePosition = new LeaguePosition(leagueEntryDTO);
             riotApiRepository.saveSetOfLeagueEntryDTO(leaguePosition);
             log.info("League Positions has been upserted successfully: " + summonerName + " " + leagueEntryDTO.getQueueType());
