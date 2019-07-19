13b870917d64400fcf1cb2f58e9880aa31cdece3
younho9 <dusgh121@gmail.com>
2019-07-19 14:01:59 +0900

    Put in LeagueEntryDTO class to LeaguePosition class

--- src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/controller/RiotApiController.java
+++ src/main/java/org/ajou/team6/riot/getleagueV4/RiotOpenAPIGetLeagueV4/controller/RiotApiController.java
@@ -1,6 +1,6 @@
 package org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.controller;
 
-import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeagueEntryDTO;
+import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.domain.LeaguePosition;
 import org.ajou.team6.riot.getleagueV4.RiotOpenAPIGetLeagueV4.service.RiotOpenApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +16,7 @@ public class RiotApiController {
     RiotOpenApiService riotOpenApiService;
 
     @GetMapping ("/riotopenapi/by-summoner-name/{summonerName}")
-    public List<LeagueEntryDTO> getLeagueEntryDTOList(@PathVariable String summonerName) {
+    public List<LeaguePosition.LeagueEntryDTO> getLeagueEntryDTOList(@PathVariable String summonerName) {
         return riotOpenApiService.getLeagueEntryDTOList(summonerName);
     }
 }
